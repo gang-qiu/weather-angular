1581f4edd55a34132a65aab137b0e25156078657
Gang Qiu <gangqiu0@gmail.com>
2020-04-03 18:52:49 -0400

    styling for side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'weather-app';
-  showMenu = true;
+  showMenu = false;
 
   toggleMenu = () => {
     this.showMenu = !this.showMenu;

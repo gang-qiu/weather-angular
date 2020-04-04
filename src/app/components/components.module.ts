b8fb7fa942d05710fdad1864ee91ce49266f0357
Gang Qiu <gangqiu0@gmail.com>
2020-04-04 00:43:47 -0400

    sets up header

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import {CommonModule} from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    SidemenuComponent
+    SidemenuComponent,
+    HeaderComponent
   ],
   exports: [
-    SidemenuComponent
+    SidemenuComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
